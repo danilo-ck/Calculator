ac7f000be167c656fe965c9a846c2dee05e3a004
danilo-ck <127243735+danilo-ck@users.noreply.github.com>
2024-08-28 21:03:49 +0200

    Add operate() function and some HTML & css
    This commit creates a new function operate() that
    takes an operator and 2 numbers and then calls one
    of the above functions on the numbers. It also
    creates a basic HTML calculator with buttons for
    each digit, each of the above functions and an
    "Equals" key.

--- javascript.js
+++ javascript.js
@@ -13,3 +13,20 @@ function multiply(num1, num2){
 function divide(dividend, divisor){
     return dividend / divisor;
 }
+
+let firstNumber, operator, secondNumber;
+
+function operate(){
+    switch(operator){
+        case "+": return add(firstNumber, secondNumber);
+        case "-": return subtract(firstNumber, secondNumber);
+        case "*": return multiply(firstNumber, secondNumber);
+        case "/": return divide(firstNumber, secondNumber);
+    }
+}
+
+let btn = document.querySelector('#one');
+
+btn.addEventListener('click',(event) => {
+    console.log('clicked');
+});
