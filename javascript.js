063a05c5b8e2171a1a64a4d561b8363718d81800
danilo-ck <127243735+danilo-ck@users.noreply.github.com>
2024-09-01 20:57:21 +0200

    Add clear feature and fix some bugs
    Calculator not evaluate more than a single pair of
    numbers at a time. Pressing = before entering all
    of the numbers or an operator not cause problems.

--- javascript.js
+++ javascript.js
@@ -17,8 +17,11 @@ function divide(dividend, divisor){
 function operate(operator, num1, num2){
     switch(operator){
         case "+": return add(num1, num2);
+        break;
         case "-": return subtract(num1, num2);
+        break;
         case "*": return multiply(num1, num2);
+        break;
         case "/": return divide(num1, num2);
     }
 }
@@ -28,7 +31,6 @@ let firstNumber = null;
 let firstOperator = null;
 let secondNumber = null;
 let secondOperator = null;
-let result = null;
 
 const numbers = document.querySelector('.numbers');
 
@@ -93,6 +95,9 @@ numbers.addEventListener('click', (event) => {
         break;
         case "equal":
             equalsTo();
+        break;
+        case "clear":
+            clearAll();
     }
 
 });
@@ -118,11 +123,49 @@ function setOperator(operator){
     if(firstOperator === null){
         firstNumber = Number(displayValue);
         firstOperator = operator;
+    }else if(secondOperator === null){
+        secondNumber = Number(displayValue);
+        secondOperator = operator;
+        firstNumber = operate(firstOperator, firstNumber, secondNumber);
+        displayValue = firstNumber;
+        display.textContent = displayValue;
+    }else {
+        secondNumber = Number(displayValue);
+        firstOperator = operator;
+        firstNumber = operate(secondOperator, firstNumber, secondNumber);
+        secondOperator = null;
+        displayValue = firstNumber;
+        display.textContent = displayValue;
     }
 }
 
 function equalsTo() {
-    secondNumber = Number(displayValue);
-    displayValue = operate(firstOperator, firstNumber, secondNumber);
+    if(secondOperator != null){
+        secondNumber = Number(displayValue);
+        displayValue = operate(secondOperator, firstNumber, secondNumber);
+        display.textContent = displayValue;
+        firstNumber = displayValue;
+        firstOperator = null;
+        secondNumber = null;
+        secondOperator = null;
+    }else if(firstOperator != null){
+        secondNumber = Number(displayValue);
+        displayValue = operate(firstOperator, firstNumber, secondNumber);
+        display.textContent = displayValue;
+        firstNumber = displayValue;
+        firstOperator = null;
+        secondNumber = null;
+        secondOperator = null;
+        //comprobar que pasa si le das a igual estando firstOperator en null al pulsar varios operadores seguidos
+    }
+
+}
+
+function clearAll(){
+    displayValue = "0";
+    firstNumber = null;
+    firstOperator = null;
+    secondNumber = null;
+    secondOperator = null;
     display.textContent = displayValue;
 }
